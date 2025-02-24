60aa3828b522fb376c60acf984da1d3fa47c4834
valeeortiz <97641033+valeeortiz@users.noreply.github.com>
2025-02-23 20:44:13 -0600

    script shh

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+echo "Apagando NGNX y NGROK..."
+sudo systemctl stop nginx
+pkill -f ngrok
+
+echo "Actualizando la aplicación..."
+cd /var/www/html || exit
+
+sudo git pull origin main  
+
+echo "Iniciando NGINX..."
+sudo systemctl start nginx
+
+echo "Iniciando NGROK..."
+nohup ngrok http 80 > /dev/null 2>&1 &
+sleep 5  # Esperar a que NGROK genere la URL
+
+echo "Obteniendo URL pública..."
+NGROK_URL=$(curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+
+echo "Accede al Resume de Valentina Ortiz: $NGROK_URL"
